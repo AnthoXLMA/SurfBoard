dc8a90cec181bda960b4ef49ae59cc23f6d7546d
AnthoXLMA <contact@service-lite-plus.com>
2020-08-29 15:04:57 +0200

    some errors

--- Gemfile
+++ Gemfile
@@ -5,12 +5,10 @@ ruby '2.6.6'
 gem 'dotenv-rails', groups: [:development, :test]
 gem 'cloudinary', '~> 1.12.0'
 gem 'geocoder'
-=======
 gem 'simple_form'
 gem 'autoprefixer-rails'
 gem 'font-awesome-sass'
 gem 'devise'
-gem 'faker'
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 6.0.3', '>= 6.0.3.2'
--- app/models/board.rb
+++ app/models/board.rb
@@ -2,8 +2,8 @@ class Board < ApplicationRecord
   has_many :rentals
   belongs_to :user
   has_one_attached :photo
-  validates :name, :height, :category, :price, presence: true
-  # validates :description, length: { maximum: 500 }
+  # validates :name, :height, :category, :price, presence: true
+  validates :description, length: { maximum: 500 }
   validates :height, inclusion: { in: %w(small medium large) }
   validates :price, numericality: true
   # geocoded_by :address
